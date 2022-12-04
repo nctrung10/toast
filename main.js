f82f01388abbc92e875c20c5753a51af7e50b428
Trung Nguyen <87252712+nctrung10@users.noreply.github.com>
2022-12-04 17:25:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ function toast({
         }
 
         const icons = {
-            success: 'fas fa-check-circle',
-            error: 'fas fa-times-circle',
-            warning: 'fas fa-exclamation-circle'
+            success: 'fa-circle-check',
+            error: 'fa-circle-xmark',
+            warning: 'fa-circle-exclamation'
         }
         const icon = icons[type]
         const delay = (duration / 1000).toFixed(2)
@@ -34,14 +34,14 @@ function toast({
 
         toast.innerHTML = `
             <div class="toast__icon">
-                <i class="${icon}"></i>
+                <i class="fa ${icon}"></i>
             </div>
             <div class="toast__body">
-                <h3 class="toast__title">${title}</h3>
-                <p class="toast__msg">${message}</p>
+                <div class="toast__title">${title}</div>
+                <p class="toast__message">${message}</p>
             </div>
             <div class="toast__close">
-                <i class="fas fa-times"></i>
+                <i class="fa fa-xmark"></i>
             </div>
         `
         main.appendChild(toast)
